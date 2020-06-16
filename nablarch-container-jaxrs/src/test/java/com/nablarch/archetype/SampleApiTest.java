afcca71fdcfabc8402126349a93ef48b754cb0a8
kumano_goro <kumano.goro@tis.co.jp>
2020-06-16 12:05:34 +0900

    XMLUnitの依存にスコープ指定追加。テストクラスの不要な定数宣言削除。

--- nablarch-container-jaxrs/src/test/java/com/nablarch/archetype/SampleApiTest.java
+++ nablarch-container-jaxrs/src/test/java/com/nablarch/archetype/SampleApiTest.java
@@ -6,7 +6,6 @@ import org.json.JSONException;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.xmlunit.builder.Input;
 
 import static org.junit.Assert.assertThat;
 import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
@@ -17,23 +16,6 @@ import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
  * @deprecated TODO 疎通確認用のクラスです。確認完了後、削除してください。
  */
 public class SampleApiTest extends RestTestSupport {
-    private static final String JSON_RESPONSE = "["
-            + "{\"userId\": 1,\"kanjiName\": \"名部楽太郎\",\"kanaName\": \"なぶらくたろう\"},"
-            + "{\"userId\": 2,\"kanjiName\": \"名部楽次郎\",\"kanaName\": \"なぶらくじろう\"}"
-            + "]";
-    private static final String XML_RESPONSE = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-            + "<userList>\n"
-            + "    <sampleUser>\n"
-            + "        <kanaName>なぶらくたろう</kanaName>\n"
-            + "        <kanjiName>名部楽太郎</kanjiName>\n"
-            + "        <userId>1</userId>\n"
-            + "    </sampleUser>\n"
-            + "    <sampleUser>\n"
-            + "        <kanaName>なぶらくじろう</kanaName>\n"
-            + "        <kanjiName>名部楽次郎</kanjiName>\n"
-            + "        <userId>2</userId>\n"
-            + "    </sampleUser>\n"
-            + "</userList>\n";
 
     /**
      * 正常終了のテストケース。
@@ -44,7 +26,10 @@ public class SampleApiTest extends RestTestSupport {
         String message = "ユーザー一覧取得（JSON）";
         HttpResponse response = sendRequest(get("/find/json"));
         assertStatusCode(message, HttpResponse.Status.OK, response);
-        JSONAssert.assertEquals(message, JSON_RESPONSE
+        JSONAssert.assertEquals(message, "["
+                        + "{\"userId\": 1,\"kanjiName\": \"名部楽太郎\",\"kanaName\": \"なぶらくたろう\"},"
+                        + "{\"userId\": 2,\"kanjiName\": \"名部楽次郎\",\"kanaName\": \"なぶらくじろう\"}"
+                        + "]"
                 , response.getBodyString(), JSONCompareMode.LENIENT);
     }
 
@@ -57,6 +42,20 @@ public class SampleApiTest extends RestTestSupport {
         String message = "ユーザー一覧取得（XML）";
         HttpResponse response = sendRequest(get("/find/xml"));
         assertStatusCode(message, HttpResponse.Status.OK, response);
-        assertThat(Input.fromString(response.getBodyString()), isSimilarTo(Input.fromString(XML_RESPONSE)));
+        assertThat(response.getBodyString()
+                , isSimilarTo("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+                        + "<userList>"
+                        + "    <sampleUser>"
+                        + "        <kanaName>なぶらくたろう</kanaName>"
+                        + "        <kanjiName>名部楽太郎</kanjiName>"
+                        + "        <userId>1</userId>"
+                        + "    </sampleUser>"
+                        + "    <sampleUser>"
+                        + "        <kanaName>なぶらくじろう</kanaName>"
+                        + "        <kanjiName>名部楽次郎</kanjiName>"
+                        + "        <userId>2</userId>"
+                        + "    </sampleUser>"
+                        + "</userList>")
+                        .ignoreWhitespace());
     }
 }
