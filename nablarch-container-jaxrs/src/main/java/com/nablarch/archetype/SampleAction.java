60b3cd92fecda2c3b2e13149b9daa7ec3e2ed088
kumagoro1202 <58413950+kumagoro1202@users.noreply.github.com>
2020-08-27 16:51:57 +0900

    ルーティングにJAX-RSのPathアノテーションを使うよう変更 (#52)

--- nablarch-container-jaxrs/src/main/java/com/nablarch/archetype/SampleAction.java
+++ nablarch-container-jaxrs/src/main/java/com/nablarch/archetype/SampleAction.java
@@ -1,20 +1,22 @@
 package com.nablarch.archetype;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import com.nablarch.archetype.dto.SampleUserListDto;
 import com.nablarch.archetype.entity.SampleUser;
-
 import nablarch.common.dao.EntityList;
 import nablarch.common.dao.UniversalDao;
 import nablarch.fw.web.HttpRequest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * 疎通確認用のアクションクラス。
  *
  * @deprecated TODO 疎通確認用のクラスです。確認完了後、削除してください。
  */
+@Path("/find")
 public class SampleAction {
 
     /**
@@ -22,9 +24,12 @@ public class SampleAction {
      * <p>
      * 応答にJSONを使用する。
      * </p>
+     *
      * @param req HTTPリクエスト
      * @return ユーザ情報(JSON)
      */
+    @GET
+    @Path("/json")
     @Produces(MediaType.APPLICATION_JSON)
     public EntityList<SampleUser> findProducesJson(HttpRequest req) {
         return UniversalDao.findAll(SampleUser.class);
@@ -35,9 +40,12 @@ public class SampleAction {
      * <p>
      * 応答にXMLを使用する。
      * </p>
+     *
      * @param req HTTPリクエスト
      * @return ユーザ情報(XML)
      */
+    @GET
+    @Path("/xml")
     @Produces(MediaType.APPLICATION_XML)
     public SampleUserListDto findProducesXml(HttpRequest req) {
         EntityList<SampleUser> sampleUserList = UniversalDao.findAll(SampleUser.class);
