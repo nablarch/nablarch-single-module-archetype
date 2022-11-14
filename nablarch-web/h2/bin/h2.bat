e5a36f1d007fa6def1553972e3c5f2ac386f0087
sukegawatakashitis <sukegawa.takashi@tis.co.jp>
2022-11-14 15:58:38 +0900

    feat: 疎通確認用のデータを更新しました。

--- nablarch-web/h2/bin/h2.bat
+++ nablarch-web/h2/bin/h2.bat
@@ -1,2 +1,2 @@
-@java -cp "h2-1.3.176.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Console %* -webAllowOthers -tcpAllowOthers
+@java -cp "h2-2.1.214.jar;%H2DRIVERS%;%CLASSPATH%" org.h2.tools.Console %* -webAllowOthers -tcpAllowOthers
 @if errorlevel 1 pause
